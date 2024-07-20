2a62c571cd54377b3da16caa9d716386e6527b2a
Alireza <shojaeealireza967@gmail.com>
2024-07-20 15:24:13 +0330

    change bg color

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 ThemeData appTheme = ThemeData(
   colorScheme: const ColorScheme.light(
-    primary: Colors.blue
+    primary: Colors.blue,
+    surface: Color(0xFFF3F7FA),
+    background: Color(0xFFF3F7FA)
   ),
   inputDecorationTheme: InputDecorationTheme(
     border: OutlineInputBorder(
